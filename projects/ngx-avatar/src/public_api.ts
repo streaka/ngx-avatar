c48a1ecf739f27fe1b5138243e342f5e15ec40a7
Josh Pike <josh@streaka.com>
2019-05-09 13:43:42 +0700

    Update public_api.ts

--- projects/ngx-avatar/src/public_api.ts
+++ projects/ngx-avatar/src/public_api.ts
@@ -3,5 +3,6 @@
  */
 export * from './lib/avatar.module';
 export * from './lib/avatar-config';
+export * from './lib/avatar.component';
 export * from './lib/avatar.service';
 export * from './lib/sources/avatar-source.enum';
